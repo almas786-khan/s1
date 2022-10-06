07f6278e23a6c21479f21516d5ad4f837c7806e0
unknown <almas786.khan@gmail.com>
2022-10-05 23:09:55 -0400

    Added basic code

---
+++ CalculatorService/CalculatorService/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CalculatorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Sub(double n1, double n2);
+        [OperationContract]
+        double Mul(double n1, double n2);
+        [OperationContract]
+        double Div(double n1, double n2);
+
+    }
+
+
+
+}
